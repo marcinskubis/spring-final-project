98ea4db6580d51703bc3decc7b5bc2cb37bfe61d
marcinskubis <marcinskubis01@gmail.com>
2024-05-23 00:43:41 +0200

    dsa

--- api/src/main/java/com/uo/springfinalproject/models/User.java
+++ api/src/main/java/com/uo/springfinalproject/models/User.java
@@ -3,11 +3,15 @@ package com.uo.springfinalproject.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
-public class User implements IMainModel{
+public class User implements IMainModel, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,4 +19,29 @@ public class User implements IMainModel{
     private String email;
     private String password;
     private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
