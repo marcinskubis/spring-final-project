821beef79fcad4ec9b2ff97dd5bb81686b1df6d1
Vankey02 <keciamamilus@gmail.com>
2024-06-02 15:08:07 +0200

    blablalba

--- filmwebclone/src/components/MovieCard.tsx
+++ filmwebclone/src/components/MovieCard.tsx
@@ -18,14 +18,15 @@ type Props = {
 export default function MovieCart({ movieItem, movies, setMovies }: Props) {
   const [movieInfo, setMovieInfo] = useState<boolean>(false);
   return (
-    <div className="flex flex-col size-80 bg-black justify-start items-center rounded-lg text-white text-lg border-2 border-white break-words">
+    <div className="flex flex-col size-80 bg-black justify-start items-center rounded-lg text-white text-lg border-2 border-white break-words whitespace-break-spaces ">
       <div className="flex  w-full h-4 grad-1"></div>
       <div className="flex  w-full h-4 grad-2"></div>
-      <div className=" inline-block line-clamp-1 font-bold mt-4 text-2xl text-yellow-400 text-center break-words w-full px-4 mt-4">
-        {movieItem.title.toUpperCase()}
+      <div className=" inline-block line-clamp-1 font-bold text-2xl text-yellow-400 text-center break-words w-full max-w-80 px-4 mt-4">
+        <p className="w-full line-clamp-1 px-4">{movieItem.title}</p>
       </div>
-      <div className="inline-block w-full line-clamp-2  break-words px-4 mt-2">
-        {movieItem.description}
+
+      <div className=" inline-block w-full mt-2">
+        <p className="w-full line-clamp-3 px-2">{movieItem.description}</p>
       </div>
       <div className="flex text-base w-full px-2 mt-6">
         <div className="opacity-60">Directed by:&nbsp; </div>
